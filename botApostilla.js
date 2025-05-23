36598a1629d2eb21b14e69b1891b140139085a5d
Cesar_Sierra <cesarmallungo@gmail.com>
2025-05-23 17:26:23 -0500

    Se actualiza archivo index.js incorporando todo el chat, se agrega funcion para validar la recepcion de pdf o img

--- botApostilla.js
+++ botApostilla.js
@@ -1,6 +1,17 @@
 const Simulaciones = require('./simulacion');
 const delay = ms => new Promise(res => setTimeout(res, ms)); 
 
+const idiomasMenu = `
+*Traducciones Oficiales*
+⚠️ Selecciona el idioma del documento:
+
+1. Inglés 🇬🇧
+2. Francés 🇫🇷
+3. Italiano 🇮🇹
+4. Alemán 🇩🇪
+5. Portugués 🇵🇹
+6. Volver al menú principal
+`;
 
 async function botApostilla(chat, client, msg, clientStates, name) {
     const simular = new Simulaciones(chat, client, msg, name);
@@ -9,7 +20,7 @@ async function botApostilla(chat, client, msg, clientStates, name) {
 
     switch (respuestaMenu) {
         case 1:
-            await simular.simulacion_corta();
+            // await simular.simulacion_corta();
             await client.sendMessage(msg.from, `Indícanos el tipo de documento que vas a apostillar:
     1. Documentos de universidad
     2. Documentos de bachillerato
@@ -22,29 +33,33 @@ async function botApostilla(chat, client, msg, clientStates, name) {
             break;
 
         case 2:
-            await client.sendMessage(msg.from, 'Traducciones oficiales: ¿Cuál es el idioma del documento que necesitas traducir?');
-            clientStates[msg.from].step = 2; // Actualizar el estado a traducciones oficiales
+            // await simular.simulacion_corta();
+            await client.sendMessage(msg.from, idiomasMenu);
+            clientStates[msg.from] = { 
+                step: 2,
+                esperandoIdioma: true // Bandera para validar la selección
+            };
             break;
 
         case 3:
             await client.sendMessage(msg.from, 'Homologación de títulos en España: Por favor, indícanos el país donde obtuviste tu título.');
-            clientStates[msg.from].step = 2; // Actualizar el estado a homologación de títulos
+            clientStates[msg.from].step = 0; // Actualizar el estado a homologación de títulos
             break;
 
         case 4:
             await client.sendMessage(msg.from, 'Asesoría Visa de estudiantes en España: ¿En qué parte de España deseas estudiar?');
-            clientStates[msg.from].step = 2; // Actualizar el estado a visa de estudiantes
+            clientStates[msg.from].step = 0; // Actualizar el estado a visa de estudiantes
             break;
 
         case 5:
             await client.sendMessage(msg.from, 'Por favor, indícanos qué otros servicios te interesan.');
-            clientStates[msg.from].step = 2; // Actualizar el estado a otros servicios
+            clientStates[msg.from].step = 0; // Actualizar el estado a otros servicios
             break;
 
         default:
             await delay(3000);
             await chat.sendStateTyping();
-            await client.sendMessage(msg.from, `¡${name}! debes seleccionar una opción válida, digita un número del 1 al 5.`);
+            await client.sendMessage(msg.from, `⚠️ ¡${name}! debes seleccionar una opción válida, digita un número del 1 al 5.`);
             break;
     }
 }
--- index.js
+++ index.js
@@ -28,6 +28,66 @@ Cesar Sierra
 
 _pd. enviar comprobante de pago_`;
 
+const menuInicial = `*Menu principal*
+    1. Apostilla de documentos.
+    2. Traducciones Oficiales.
+    3. Homologación de títulos en España.(mantenimiento)
+    4. Asesoría Visa de estudiantes España.(mantenimiento)
+    5. Otros Servicios
+    6. Salir.`
+
+const idiomasMenu = `*Traducciones Oficiales*
+Selecciona el idioma del documento:
+
+1. Inglés 🇬🇧
+2. Francés 🇫🇷
+3. Italiano 🇮🇹
+4. Alemán 🇩🇪
+5. Portugués 🇵🇹
+6. Volver al menú principal
+`;
+const idiomas = {
+    1: "Inglés",
+    2: "Francés",
+    3: "Italiano",
+    4: "Alemán",
+    5: "Portugués"
+};
+
+const menuApostillaDocumentos = `
+    1. Documentos de universidad
+    2. Documentos de bachillerato
+    3. Antecedentes Judiciales
+    4. Registros Civiles
+    5. Apostilla de documentos en España
+    6. Otro tipo de apostillas
+    7. Volver al menú principal
+`;
+
+async function  pdfComprobante(client, msg, clientStates, mediosDePago) {
+    if (msg.type === 'document' || msg.type === 'image') {
+        await client.sendMessage(msg.from,
+            `📄 *¡Archivo recibido!*\nExcelente trabajo,en seguida te contactaremos con nuestro equipo para finalizar el proceso.\nSí necesita cotizar otro servicio escriba *Menu principal* o *hola*._Gracias por elegir nuestros servicios_ 👍`
+        );
+        delete clientStates[msg.from]; // Resetea estado
+    } else {
+        // Incrementa los intentos
+        clientStates[msg.from].intentos += 1;
+
+        // Si supera los 3 intentos, cancela el proceso
+        if (clientStates[msg.from].intentos >= 3) {
+            await client.sendMessage(msg.from,
+                "❌ Has enviado varios mensajes incorrectos. Por favor, inicia el proceso nuevamente escribiendo *Hola* o contacta asistencia."
+            );
+            delete clientStates[msg.from]; // Cancelar flujo
+        } else {
+            // Aún tiene intentos restantes
+            await client.sendMessage(msg.from,
+                "⚠️ Por favor, envía *solo archivos PDF*.\nSi conoce el monto a pagar, envie el comprobante de pago para agilizar el proceso, de lo contrario, envíe el archivo en pdf y espere su cotización."
+            );
+        }
+    }
+}
 
 
 // Diccionario para mantener el estado de las conversaciones y ultimo msj
@@ -38,11 +98,10 @@ client.on('message', async msg => {
     // Obtener el chat y el contacto de la conversación actual
     const chat = await msg.getChat();
     const contact = await msg.getContact();
-    const name = contact.pushname ? contact.pushname.split(" ")[0] : 'Usuario'; // Si no hay pushname, usa 'Usuario' por defecto
+    const name = contact.pushname ? contact.pushname.split(" ")[0] : ''; // Si no hay pushname, usa 'Usuario' por defecto
 
     //instancia de la clase Simulaciones
     const simular = new Simulaciones(chat, client, msg, name);
-
 
     // Estado inicial de la conversación si no existe
     if (!clientStates[msg.from]) {
@@ -50,162 +109,197 @@ client.on('message', async msg => {
     }
 
     let currentState = clientStates[msg.from].step;
+    const estado = clientStates[msg.from];
 
-
-
-    // Si el mensaje contiene alguna palabra clave inicial
-    if (currentState === 0 && /interesado|hola|información|buenas|buen|más información|cordial|como estas/i.test(msg.body)) {
-        await simular.simulacion_corta();
-        await client.sendMessage(msg.from, `¡Saludos ${name}! Sea bienvenido a *Apostilla Colombia Traducciones Oficiales*.`);
-        await simular.simulacion_media();
-        await client.sendMessage(msg.from, `¿Cuéntanos, en qué servicio estás interesado?:
-    1. Apostilla de documentos.
-    2. Traducciones Oficiales.
-    3. Homologación de títulos en España.
-    4. Asesoría Visa de estudiantes España.
-    5. Otros.`);
-
-        clientStates[msg.from].step = 1; // Actualizar el estado al menú principal 1
+    // Estado de la conversación esperando PDF o imagen
+    if (estado && estado.step === 'esperando_archivo') {
+        await pdfComprobante(client, msg, clientStates, mediosDePago);
         return;
     }
 
-    // Procesar la respuesta del menú principal
+    //Estado de la conversación 0 - Esperando mensaje
+    if (currentState === 0 && /interesado|hola|información|buenas|buen|más información|Menu principal||Menú principal|cordial|como estas/i.test(msg.body)) {
+        // await simular.simulacion_corta();
+        await client.sendMessage(msg.from, `¡Saludos ${name}! 
+Sea bienvenido a *Apostilla Colombia Traducciones Oficiales*.`);
+        // await simular.simulacion_media();
+        await client.sendMessage(msg.from, `¿Cuéntenos, en qué servicio está interesado?
+${menuInicial}`);
+
+        clientStates[msg.from].step = 1; // Actualizar el estado 1 - Menú principal
+        return;
+    }
+
+    //Estado de la conversación 1 - Menú principal
     if (currentState === 1) {
-        await botApostilla(chat, client, msg, clientStates, name);
-        return;
-    }
+        const simular = new Simulaciones(chat, client, msg, name);
 
-    // Ejemplo de cómo seguir interactuando según el estado (documentos de apostilla en este caso)
-    if (currentState === 2) {
+        const respuestaMenuPrincipal = parseInt(msg.body);
 
-        if (msg.type === 'image') {
-
-            await simular.simulacion_corta();
-            await client.sendMessage(msg.from, `Vamos a validar la información y en seguida nos pondremos en contacto contigo.`);
-            clientStates[msg.from].step = 0; 
-        } else if (/SALIR/i.test(msg.body)) {
-            await client.sendMessage(msg.from, `Gracias por su visita.`);
-            clientStates[msg.from].step = 0; 
-        } else {
-            await client.sendMessage(msg.from, `Por favor, envía una imagen para continuar.
-o escribe *SALIR* para terminar.`);
-        }
-    }
-
-    if (currentState === 3) {
-        const respuestaMenuApostilla = parseInt(msg.body); // Convertir a número la opción
-
-        switch (respuestaMenuApostilla) {
-            case 1:
-                await chat.sendSeen();
-                await delay(3000);
-                await chat.sendStateTyping();
-                await delay(5000);
-                await chat.clearState();
-                await client.sendMessage(msg.from, `Valor $80.000 por documento,
-debe adjuntar los documentos en PDF de manera individual y con buena resolución, 
-tiempo estimado de entrega 3-5 días hábiles,
-
-las apostillas las enviamos por este medio o al correo.`);
-                await chat.sendStateTyping();
-                await delay(3000);
-                await client.sendMessage(msg.from, `${mediosDePago}`);
-                clientStates[msg.from].step = 2; // Actualizar el estado a documentos de apostilla
+        switch (respuestaMenuPrincipal) {
+            case 1: // Apostilla de documentos
+                // await simular.simulacion_corta();
+                await client.sendMessage(msg.from, `*Apostilla de documentos*\nIndícanos el tipo de documento que vas a apostillar:\n${menuApostillaDocumentos}`);
+                clientStates[msg.from].step = 2; // Actualizar al estado 2 - Apostilla de documentos
                 break;
 
-            case 2:
-                await chat.sendSeen();
-                await delay(2000);
-                await chat.sendStateTyping();
-                await delay(2000);
-                await client.sendMessage(msg.from, `Colegios de bogota
-Valor $80.000 por documento,
-debe adjuntar los documentos en PDF de manera individual y con buena resolución, 
-tiempo estimado de entrega 15 días,
-
-otros colegios
-legalizacion y apostilla
-Valor $160.000 por documento,
-debe adjuntar los documentos en PDF de manera individual y con buena resolución, 
-tiempo estimado de entrega 15 días
-
-las apostillas las enviamos por este medio o al correo.`);
-                await chat.sendStateTyping();
-                await delay(3000);
-                await client.sendMessage(msg.from, `${mediosDePago}`);
-                clientStates[msg.from].step = 2; // Actualizar el estado a traducciones oficiales
+            case 2: // TRADUCCIONES OFICIALES
+                // await simular.simulacion_corta();
+                await client.sendMessage(msg.from, idiomasMenu);
+                clientStates[msg.from] = {
+                    step: 3, // Actualizar al estado 3 - Traducciones oficiales
+                    esperandoIdioma: true // Bandera para validar la selección
+                };
                 break;
 
-            case 3:
-                await chat.sendSeen();
-                await delay(2000);
-                await chat.sendStateTyping();
-                await delay(2000);
-                await client.sendMessage(msg.from, `Colegios de bogota
-Valor $80.000 por antecedente,
-debe adjuntar la CC(Cédula Ciudadania) en PDF de manera individual y con buena resolución, 
-tiempo estimado de entrega 15 días,
-
-las apostillas las enviamos por este medio o al correo.`);
-                await chat.sendStateTyping();
-                await delay(3000);
-                await client.sendMessage(msg.from, `${mediosDePago}`);
-                clientStates[msg.from].step = 2; // Actualizar el estado a homologación de títulos
+            case 3: // HOMOLOGACIÓN DE TÍTULOS
+                await client.sendMessage(msg.from, 'Homologación de títulos en España: Por favor, indícanos el país donde obtuviste tu título.');
+                clientStates[msg.from].step = 4; // Actualizar el estado a homologación de títulos
                 break;
 
             case 4:
-                await chat.sendSeen();
-                await delay(2000);
-                await chat.sendStateTyping();
-                await delay(2000);
-                await client.sendMessage(msg.from, `Indiquenos lo siguiente:
-¿Que entidad expidío el documento?
-1. Registraduria Nacional (Auxiliares o principales)
-Valor $120.000, debe adjuntar el documento en pdf de manera individual y 
-tiempo de entrega 1 dias.
-2. Notarias Colombianas 
-Valor $80.000 debe compartir el codigo de apostilla (solicitarlo en la notaria)
-tiempo de entrega 1 día.
-
-*Nota* 
-_Los documento de registraduria vienen con un holograma o stiker plateado_.`);
-                await chat.sendStateTyping();
-                await delay(3000);
-                await client.sendMessage(msg.from, `${mediosDePago}`);
-                clientStates[msg.from].step = 2; // Actualizar el estado a homologación de títulos
+                await client.sendMessage(msg.from, 'Asesoría Visa de estudiantes en España: ¿En qué parte de España deseas estudiar?');
+                clientStates[msg.from].step = 0; // Actualizar el estado a visa de estudiantes
                 break;
 
             case 5:
-                await client.sendMessage(msg.from, 'apostilla españa.');
-                clientStates[msg.from].step = 2; // Actualizar el estado a otros servicios
-                break;
-
-            case 6:
                 await client.sendMessage(msg.from, 'Por favor, indícanos qué otros servicios te interesan.');
-                clientStates[msg.from].step = 2; // Actualizar el estado a otros servicios
-                break;
-
-            case 7:
-                await client.sendMessage(msg.from, `¿Cuéntanos, en qué servicio estás interesado?:
-    1. Apostilla de documentos.
-    2. Traducciones Oficiales.
-    3. Homologación de títulos en España.
-    4. Asesoría Visa de estudiantes España.
-    5. Otros.`);
-                clientStates[msg.from].step = 1; 
+                clientStates[msg.from].step = 0; // Actualizar el estado a otros servicios
                 break;
 
             default:
                 await delay(3000);
                 await chat.sendStateTyping();
-                await client.sendMessage(msg.from, `¡${name}! debes seleccionar un opción valida, digite un número del 1 al 6`);
+                await client.sendMessage(msg.from, `⚠️ *Menú pricipal*\n¡${name}! debes seleccionar una opción válida, digita un número del 1 al 5.`);
                 break;
         }
         return;
-        
+    }
+
+    //Estado de la conversación 2 - Apostilla de documentos
+    if (currentState === 2) {
+        const respuestaMenuApostilla = parseInt(msg.body);
+
+        switch (respuestaMenuApostilla) {
+            case 1:
+                await client.sendMessage(msg.from, `*Apostilla de documentos*
+Documentos de universidad:
+Valor $80.000 por documento,
+debe adjuntar los documentos en *PDF* de manera individual con buena resolución.
+Tiempo estimado de respuesta 3-5 días hábiles,
+
+las apostillas las enviamos por este medio o al correo.`);
+
+                await client.sendMessage(msg.from, `${mediosDePago}`);
+
+                // Actualiza el estado del cliente para esperar el PDF o imagen
+                clientStates[msg.from] = {
+                    step: 'esperando_archivo',
+                    intentos: 0
+                };
+                break;
+
+            case 2:
+                await client.sendMessage(msg.from, `*Apostilla de documentos*
+Colegios de bogota:
+legalizacion y apostilla
+Valor $80.000 por documento,
+debe adjuntar los documentos en PDF de manera individual con buena resolución. 
+tiempo estimado de respuesta 15 días calendario.
+
+Otras ciudades:
+legalizacion y apostilla
+Valor $180.000 por documento,
+debe adjuntar los documentos en PDF de manera individual con buena resolución, 
+tiempo estimado de respuesta 15 días calendario.
+
+las apostillas las enviamos por este medio o al correo.`);
+                await pdfComprobante(client, msg, clientStates, mediosDePago);
+                break;
+
+            case 3:
+                await client.sendMessage(msg.from, `*Apostilla de documentos*
+Antecedentes Judiciales con fines migratorios
+Valor $80.000 por antecedente,
+debe adjuntar la CC(Cédula Ciudadania) en *PDF* con buena resolución, 
+tiempo estimado de respuesta 1 día.
+
+las apostillas las enviamos por este medio o al correo.`);
+                await client.sendMessage(msg.from, `${mediosDePago}`);
+                break;
+
+            case 4:
+
+                await client.sendMessage(msg.from, `*Apostilla de documentos*
+Registros Civiles: (valide su documento)
+    1. Registraduria Nacional (Auxiliares o principales)
+    Valor $120.000 por documento, debe adjuntar el documento en pdf de manera individual, tiempo de respues 15 días calendario.
+    2. Notarias Colombianas 
+    Valor $80.000 por documento, debe compartir el codigo de apostilla (solicitarlo previamente en la notaria) tiempo de entrega 1 día habil.
+
+    *Nota* 
+    _Los registros de registraduria vienen con un holograma o stiker plateado_.`);
+                await client.sendMessage(msg.from, `${mediosDePago}`);
+                break;
+
+            case 5:
+                await client.sendMessage(msg.from, 'apostilla españa.');
+                clientStates[msg.from].step = 0; // Actualizar el estado a otros servicios
+                break;
+
+            case 6:
+                await client.sendMessage(msg.from, 'Por favor, indícanos qué otros servicios te interesan.');
+                clientStates[msg.from].step = 0; // Actualizar el estado a otros servicios
+                break;
+
+            case 7:
+                await client.sendMessage(msg.from, "📌 Volviendo al menú principal...");
+                await client.sendMessage(msg.from, menuInicial);
+                clientStates[msg.from].step = 1;
+                break;
+
+            default:
+
+                await client.sendMessage(msg.from, `⚠️ ¡${name}! debes seleccionar un opción valida, digite un número del 1 al 6`);
+                break;
+        }
+        return;
+
     }
 
 
+
+    //Estado de la conversación 3 - Traducciones oficiales
+    if (currentState === 3 && clientStates[msg.from]?.esperandoIdioma) {
+        const opcion = parseInt(msg.body.trim());
+
+        if (opcion >= 1 && opcion <= 5) {
+            const idiomaElegido = idiomas[opcion];
+            clientStates[msg.from] = {
+                step: 3,
+                idiomaDocumento: idiomaElegido,
+                esperandoDocumento: true
+            };
+            // await simular.simulacion_corta();
+            await client.sendMessage(msg.from, `*Traducciones Oficiales*\n✅ Idioma seleccionado *${idiomaElegido}*. a continucación adjunte el archivo en *PDF* con buena resolución para indicarle la cotización de la traducción.`
+            );
+
+            clientStates[msg.from] = {
+                step: 'esperando_archivo', // Actualizar el estado para esperar el PDF o imagen
+                intentos: 0
+            };
+        }
+        else if (opcion === 6) {
+            await client.sendMessage(msg.from, `📌 Volviendo al menú principal...`);
+            await client.sendMessage(msg.from, menuInicial);
+            clientStates[msg.from].step = 1;
+        }
+        else {
+            await client.sendMessage(msg.from, `*Traducciones Oficiales*
+    ❌ Opción inválida. Por favor, escribe un número del 1 al 6 y seleccione su idioma.\n_${idiomas}_`
+            );
+        }
+    }
 
 
 });
